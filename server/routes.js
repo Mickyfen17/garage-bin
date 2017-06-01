66a3de0206d2fa826c6232366a18a851100744b0
Michael Fenwick <mickyfen17@aol.com>
2017-06-01 14:11:23 -0600

    add patch endpoint

--- server/routes.js
+++ server/routes.js
@@ -18,7 +18,7 @@ router.get('/garage', (request, response) => {
 
 router.get('/garage/:id', (request, response) => {
   const { id } = request.params;
-  
+
   database('garage').where('id', id)
     .then((item) => {
       if (!item.length) {
@@ -29,6 +29,29 @@ router.get('/garage/:id', (request, response) => {
     })
     .catch((error) => {
       response.status(500).send({ error });
+    });
+});
+
+router.patch('/garage/item/:id', (request, response) => {
+  const { id } = request.params;
+  const { cleanliness } = request.body;
+
+  if (!cleanliness) { return response.status(422).send({ error: 'Missing content from patch' }); }
+
+  database('garage').where('id', id).select()
+    .then((data) => {
+      if (!data.length) {
+        response.status(404).send({ error: 'Invalid Item ID' });
+      } else {
+        database('garage').where('id', id)
+        .update({ cleanliness }, ['id', 'name', 'reason', 'cleanliness'])
+        .then((updatedItem) => {
+          response.status(200).send(...updatedItem);
+        })
+        .catch((error) => {
+          response.status(500).send({ error });
+        });
+      }
     });
 });
 
