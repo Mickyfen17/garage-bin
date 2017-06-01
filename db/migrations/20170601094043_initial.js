563f6a5af6ee8f1e0fdec368862a8e7d2feb33d9
Michael Fenwick <mickyfen17@aol.com>
2017-06-01 11:12:53 -0600

    remove unique key

--- db/migrations/20170601094043_initial.js
+++ db/migrations/20170601094043_initial.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('garage', (table) => {
       table.increments('id').primary();
-      table.string('name').unique();
+      table.string('name');
       table.string('reason');
       table.string('cleanliness');
 
