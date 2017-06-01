8289f6ed351cb4c7aaa1168f49ed877534cc4976
Michael Fenwick <mickyfen17@aol.com>
2017-06-01 09:38:33 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.set('port', process.env.PORT || 3000);
+
+app.use('/api/v1', routes);
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`Server is listening on ${app.get('port')}.`)
+  });
+}
+
+module.exports = app;
