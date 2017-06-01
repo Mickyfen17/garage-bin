0e56aa8850071868e07dc639f9c8baa444081916
Michael Fenwick <mickyfen17@aol.com>
2017-06-01 10:06:33 -0600

    add first get all request for garage items

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,19 @@
 const express = require('express');
+
 const router = express.Router();
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
-
-
+router.get('/garage', (request, response) => {
+  database('garage').select()
+    .then((allItems) => {
+      response.status(200).json(allItems);
+    })
+    .catch((error) => {
+      response.status(500).send({ error });
+    });
+});
 
 module.exports = router;
